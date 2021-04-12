11e3934d66ae11e8f4f5e846bbf84673ad1743af
kiran <007jkiran@gmail.com>
2021-04-12 18:18:18 +0530

    intial commit

---
+++ src/components/Error.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const Error = () => {
+  return <div className="section section-center text-center"><h3>there was error</h3></div>
+}
+
+export default Error
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+import About from "./AboutPage";
+import AuthWrapper from './AuthWrapper';
+
+export { Home, Products, SingleProduct,  Cart, Error,Checkout, Private, About, AuthWrapper };
